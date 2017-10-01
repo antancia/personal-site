6713ebbde4851cf801803a1486e6862be0c07ee8
Harmony D <antancia@gmail.com>
2017-10-01 15:02:53 -0400

    sidebar kinda works

---
+++ src/components/SidebarContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SidebarContent = () => {
+  return (
+    <div className="SidebarContent">
+      <p>Sidebar stuff is here</p>
+    </div>
+  );
+}
+
+export default SidebarContent;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as App } from './App';
 export { default as Home } from './Home';
-export { default as Sidebar } from './Sidebar';
+export { default as SidebarContent } from './SidebarContent';
 export { default as Carousel } from './Carousel';
 export { default as About } from './About';
