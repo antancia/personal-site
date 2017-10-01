2abf18b27a0d7abca7f8190440437349983f3b34
Harmony D <antancia@gmail.com>
2017-10-01 15:57:05 -0400

    sidebar styles into their own file

--- src/components/SidebarContent.js
+++ src/components/SidebarContent.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-const styles = {
-  sidebar: {
-    width: 256,
-    height: '100%',
-  },
-  sidebarLink: {
-    display: 'block',
-    padding: '16px 0px',
-    color: '#757575',
-    textDecoration: 'none',
-  },
-  divider: {
-    margin: '8px 0',
-    height: 1,
-    backgroundColor: '#757575',
-  },
-  content: {
-    padding: '16px',
-    height: '100%',
-    backgroundColor: 'white',
-  },
-};
+import '../styles/SidebarContent.css';
 
 const SidebarContent = () => {
   return (
-    <div className="SidebarContent" style={styles.content}>
+    <div className="SidebarContent">
       <p>Sidebar stuff is here</p>
     </div>
   );
