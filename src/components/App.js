6c2bfd8dda90e244ba993440e634ef364fe578c4
Harmony D <antancia@gmail.com>
2017-10-01 16:11:38 -0400

    Add the sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,72 @@
 import React from 'react';
 import '../styles/App.css';
-import Sidebar from './Sidebar';
+import SidebarContent from './SidebarContent';
+import Sidebar from 'react-sidebar';
 
-const App = props => {
-  return (
-    <div className="App">
-      <Sidebar />
-      { props.children }
-    </div>
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      mql: mql,
+      docked: false,
+      open: false,
+    };
+
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.onSetOpen = this.onSetOpen.bind(this);
+  }
+
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+    this.setState({mql: mql, docked: mql.matches});
+  }
+
+  componentWillUnmount() {
+    this.state.mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetOpen(open) {
+    this.setState({open: open});
+  }
+
+  mediaQueryChanged() {
+    this.setState({
+      mql: mql,
+      docked: this.state.mql.matches,
+    });
+  }
+
+  toggleOpen(ev) {
+    this.setState({open: !this.state.open});
+
+    if (ev) {
+      ev.preventDefault();
+    }
+  }
+
+  render() {
+    const sidebar = <SidebarContent onSetOpen={this.onSetOpen} />;
+
+    return (
+      <div className="App">
+        <Sidebar
+          sidebar={sidebar}
+          open={this.state.open}
+          docked={this.state.docked}
+          onSetOpen={this.onSetOpen}
+          shadow={false}
+        >
+          {!this.state.docked &&
+           <a onClick={this.toggleOpen} href="#">=</a>}
+          { this.props.children }
+        </Sidebar>
+      </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/SidebarContent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/SidebarContent.css';
+
+const SidebarContent = (props) => {
+  function closeSidebar() {
+    props.onSetOpen(false);
+  }
+
+  return (
+    <div className="SidebarContent">
+      <div>
+        <h2>Harmony Dashut</h2>
+      </div>
+      <div>
+        <Link to="/" onClick={closeSidebar}>Home</Link>
+        <br />
+        <Link to="/photos" onClick={closeSidebar}>Photos</Link>
+        <br />
+        <Link to="/about" onClick={closeSidebar}>About Me</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SidebarContent;
