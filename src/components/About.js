4ea77ebfdc8ed405b0d3b2457ea57126c3a13b99
Harmony D <antancia@gmail.com>
2017-10-07 17:38:52 -0400

    some work on about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../styles/About.css'
 
 const About = () => {
   return (
     <div className="About">
-      <p>Some stuff about me</p>
+      <p>I'm a photographer, maker, and developer. I created this website
+      using React. Currently I live in Brooklyn. Contact me using various social
+      media.</p>
+      <a href="https://www.instagram.com/harmony_dashut/">
+        <i className="fa fa-instagram icon" aria-hidden="true"></i>
+      </a>
+      <a href="https://twitter.com/harmonydashut">
+        <i className="fa fa-twitter-square icon" aria-hidden="true"></i>
+      </a>
     </div>
   );
 }
